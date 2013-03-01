4db4da382072fd31ba145e452279194a2f02afc0
Finn Pauls <derfinn@gmail.com>
2013-03-02 00:21:51 +0200

    dev causing problems?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='simple4sq',
-    version='0.1dev',
+    version='0.1',
     packages=['simple4sq'],
     install_requires=['requests'],
     license='MIT License',
